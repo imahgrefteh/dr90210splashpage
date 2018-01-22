2b8f57ef44aaec7d504cb431aace7dd97cebbe45
isac_personnal <imahgrefteh@yahoo.com>
2018-01-21 20:08:21 -0800

    fixing port for heroku

--- config.js
+++ config.js
@@ -7,5 +7,5 @@ config.gmail.password = process.env.GMAIL_PASSWORD || "PASSWORD";
 config.gmail.to = process.env.GMAIL_TO || "TO_ADDRESS";
 
 config.web = {};
-config.web.port = process.env.WEB_PORT || 8080;
+config.web.port = process.env.PORT || 8080;
 module.exports = config;
