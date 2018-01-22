0db64b53d6070c44e1e07acc56a3a1087ee0a851
isac_personnal <imahgrefteh@yahoo.com>
2018-01-21 19:46:09 -0800

    genric config file

--- config.js
+++ config.js
@@ -1,1 +1,10 @@
-var config = {}
+var config = {};
+config.gmail = {};
+
+config.gmail.user_name = process.env.gmail.user_name || "USERNAME";
+config.gmail.password = process.env.gmail.password || "PASSWORD";
+config.gmail.to = process.env.gmail.to || "TO_ADDRESS";
+
+config.web = {};
+config.web.port = process.env.WEB_PORT || 8080;
+module.exports = config;
